50f3bfa91f740f4b6e16e9b8c7259134c9d9515b
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-07-26 16:58:11 -0700

    scraping new egg website for graphics cards

---
+++ ScrapingNewEggGraphicsCards.py
@@ -0,0 +1,5 @@
+import bs4
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
