b6e725858de4c5504167e4df91e56c50a63e850b
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2017-07-26 17:41:19 -0700

    parsing the page and grabbing containers

--- ScrapingNewEggGraphicsCards.py
+++ ScrapingNewEggGraphicsCards.py
@@ -3,3 +3,13 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
 my_url = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card'
+#opening the connection and grabbing the webpage
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+#parse the page as html 
+page_soup = soup(page_html,"html.parser")
+
+#grabs each product
+containers = page_soup.findAll("div",{"class": "item-container"})
